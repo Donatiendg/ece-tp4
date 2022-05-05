7786f41c77cc764899b6c5d5431e2c5d6e90242a
donatien de gervillier <donatien.dg@outlook.com>
2022-05-05 20:11:04 +0200

    placement des pions

--- assets/js/moteur.js
+++ assets/js/moteur.js
@@ -92,5 +92,41 @@ console.log(elements)
 console.log(elements.length)
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', play, false);
-    document.createTextNode("test")
+    document.createTextNode("test");
+    if (i === 0 || i === 7){
+        elements[i].classList.add("tour-noir");
+    }
+    else if (i === 1 || i === 6){
+        elements[i].classList.add("cavalier-noir");
+    }
+    else if (i === 2 || i === 5){
+        elements[i].classList.add("fou-noir");
+    }
+    else if (i === 4){
+        elements[i].classList.add("roi-noir");
+    }
+    else if (i === 5){
+        elements[i].classList.add("reine-noir");
+    }
+    else if (i > 6 && i < 16){
+        elements[i].classList.add("pion-noir");
+    }
+    if (i === 48 || i === 55){
+        elements[i].classList.add("tour-blanc");
+    }
+    else if (i === 49 || i === 54){
+        elements[i].classList.add("cavalier-blanc");
+    }
+    else if (i === 50 || i === 53){
+        elements[i].classList.add("fou-blanc");
+    }
+    else if (i === 51){
+        elements[i].classList.add("reine-blanc");
+    }
+    else if (i === 52){
+        elements[i].classList.add("roi-blanc");
+    }
+    else if (i > 39 && i < 48){
+        elements[i].classList.add("pion-blanc");
+    }
 }
