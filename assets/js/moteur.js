a48f4b720574c6727bc20240c9df9fb87ae7583c
donatien de gervillier <donatien.dg@outlook.com>
2022-05-06 12:46:52 +0200

    déplacement des pions

--- assets/js/moteur.js
+++ assets/js/moteur.js
@@ -1,19 +1,19 @@
-var currentPlayer = 1;
-var scorePlayer1 = 0;
-var scorePlayer2 = 0;
+let currentPlayer = 1;
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
 
 /**
  * Variables liées à la sélection
  */
-var currentSelection = false;
-var selectedHTML;
-var selectedHTMLClasses;
+let currentSelection = false;
+let selectedHTML;
+let selectedHTMLClasses;
 
 /**
  * Récupère la deuxième classe déclaré dans votre liste.
  * Découpe votre ensemble de classe selon les espaces pour les mettre dans un tableau (split).
  * @param {*} selectedClasses 
- * @returns la classe liée à une pièce si elle exise sinon renvoie vide.
+ * @returns la classe liée à une pièce si elle existe sinon renvoie vide.
  */
 function getCaseClass(selectedClasses) {
     /**
@@ -21,7 +21,7 @@ function getCaseClass(selectedClasses) {
      * Note : Toujours déclarer votre class liée à une pièce en deuxième.
      */
     if (selectedClasses !== "") {
-        var arraySplitedClasses = selectedClasses.split(" ");
+        let arraySplitedClasses = selectedClasses.split(" ");
         if (arraySplitedClasses.length > 1) {
             return arraySplitedClasses[1];
         } else {
@@ -34,9 +34,9 @@ function getCaseClass(selectedClasses) {
  * Passe au joueur suivant.
  */
 function changePlayer() {
-    if (currentPlayer == 1) {
+    if (currentPlayer === 1) {
         currentPlayer = 2;
-    } else if (currentPlayer == 2) {
+    } else if (currentPlayer === 2) {
         currentPlayer = 1;
     }
 }
@@ -46,9 +46,9 @@ function changePlayer() {
  * @param {*} classList liste des classes d'un élément HTML.
  */
 function addSelectedClassByPlayer(classList) {
-    if (currentPlayer == 1) {
+    if (currentPlayer === 1) {
         classList.add("selectedRed");
-    } else if (currentPlayer == 2) {
+    } else if (currentPlayer === 2) {
         classList.add("selectedBlue");
     }
 }
@@ -58,10 +58,10 @@ function addSelectedClassByPlayer(classList) {
  * @param {*} classList liste des classes d'un élément HTML.
  */
 function removeSelectedClassByPlayer(classList) {
-    if (currentPlayer == 1) {
-        //classList.remove(votreClass);
-    } else if (currentPlayer == 2) {
-        //classList.remove(votreClass);
+    if (currentPlayer === 1) {
+        classList.remove("selectedRed");
+    } else if (currentPlayer === 2) {
+        classList.remove("selectedBlue");
     }
 }
 
@@ -69,28 +69,84 @@ function removeSelectedClassByPlayer(classList) {
  * Fonction liée à l'évènement 'click'.
  * A MODIFIER
  */
-var play = function() {
+let play = function() {
+    //console.log(currentPlayer);
     if (!currentSelection) {
         selectedHTML = this;
         selectedHTMLClasses = this.className;
-        var piece = getCaseClass(selectedHTMLClasses)
-        currentSelection = true;
-        var classPiece = getCaseClass(selectedHTMLClasses);
-        addSelectedClassByPlayer(this.classList);
-        console.log("??")
+        let piece = getCaseClass(selectedHTMLClasses);
+        //console.log(isCaseEmpty(piece));
+        //console.log(isCaseAllowed(piece));
+        if (isCaseEmpty(piece) && isCaseAllowed(piece)){
+            currentSelection = true;
+            addSelectedClassByPlayer(this.classList);
+        }
+        //var classPiece = getCaseClass(selectedHTMLClasses);
     } else {
+        let piece = getCaseClass(this.className);
+        removeSelectedClassByPlayer(selectedHTML.classList);
 
-
+        if (this === selectedHTML || isCaseAllowed(piece)){//Impossible de poser le pion
+            currentSelection = false;
+        }
+        else if(!isCaseEmpty(piece)){//déplacer sur une case vide
+            this.classList.add(getCaseClass(selectedHTMLClasses));
+            selectedHTML.classList.remove(getCaseClass(selectedHTMLClasses));
+            selectedHTML = null;
+            selectedHTMLClasses = null;
+            currentSelection = false;
+            isWin();
+            changePlayer();
+        }
+        else {//Poser sur un pion adverse
+            this.classList.add(getCaseClass(selectedHTMLClasses));
+            this.classList.remove(piece);
+            selectedHTML.classList.remove(getCaseClass(selectedHTMLClasses));
+            selectedHTML = null;
+            selectedHTMLClasses = null;
+            currentSelection = false;
+            isWin();
+            changePlayer();
+        }
     }
 };
 
+function getElementsChildren(selectedClasses){
+    return selectedClasses.childNodes;
+}
+
+function isCaseEmpty(selectedClasses){
+    return getCaseClass(selectedClasses) === '';
+}
+
+function isCaseAllowed(selectedClasses){
+    if (currentPlayer === 1){
+        return arrayPlayer1Pieces.includes((selectedClasses));
+    }
+    else if (currentPlayer === 2){
+        return arrayPlayer2Pieces.includes((selectedClasses));
+    }
+}
+
+function isWin(){
+    let grid = document.getElementsByClassName("case");
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        if (arrayPlayer1Pieces.includes(getCaseClass(grid[i].className))){
+            count ++;
+        }
+    }
+    console.log(count);
+    console.log(arrayPlayer1Pieces.includes(getCaseClass(grid[0].className)));
+}
+
+let arrayPlayer1Pieces = ["tour-noir", "cavalier-noir", "fou-noir", "roi-noir", "reine-noir", "pion-noir"];
+let arrayPlayer2Pieces = ["tour-blanc", "cavalier-blanc", "fou-blanc", "roi-blanc", "reine-blanc", "pion-blanc"];
 /**
  * On lie tous les éléments avec la class 'case' à l'événement 'click'.
  */
 var elements = document.getElementsByClassName("case");
-console.log(elements)
-console.log(elements.length)
-for (var i = 0; i < elements.length; i++) {
+for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', play, false);
     document.createTextNode("test");
     if (i === 0 || i === 7){
@@ -105,7 +161,7 @@ for (var i = 0; i < elements.length; i++) {
     else if (i === 4){
         elements[i].classList.add("roi-noir");
     }
-    else if (i === 5){
+    else if (i === 3){
         elements[i].classList.add("reine-noir");
     }
     else if (i > 6 && i < 16){
